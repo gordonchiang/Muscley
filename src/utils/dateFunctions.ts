0c1f64610a002192a131c9e03c0dedebdbc10b9e
Gordon Chiang <gordonnnnnnnnnnn@gmail.com>
2023-02-05 01:25:41 -0800

    Update docstring

--- src/utils/dateFunctions.ts
+++ src/utils/dateFunctions.ts
@@ -1,6 +1,8 @@
-/*
+/**
  * Converts a date object into YYYY-MM-DD string format.
- */
+ * @param {Date} date A date object.
+ * @returns {string} The local date formatted as a YYYY-MM-DD string. 
+ **/
 export const dateObjectToString = (date: Date): string => {
   const year: string = date.getFullYear().toString();
   const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
